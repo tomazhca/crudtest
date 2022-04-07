bb1cfde833e2d42d3ab4d9db38b83459d31c9d77
Tomaz Araujo <tomazhca@gmail.com>
2022-04-07 00:50:26 -0300

    Update ClientService

--- backend/src/main/java/com/tomazhca/crudtest/services/ClientService.java
+++ backend/src/main/java/com/tomazhca/crudtest/services/ClientService.java
@@ -40,7 +40,7 @@ public class ClientService {
 	@Transactional
 	public ClientDTO insert(ClientDTO dto) {
 		Client entity = new Client();
-		entity.setName(dto.getName());
+		copyEntityDTO(entity, dto);
 		entity = repository.save(entity);
 		return new ClientDTO(entity);
 	}
@@ -48,8 +48,8 @@ public class ClientService {
 	@Transactional
 	public ClientDTO update(Long id, ClientDTO dto) {
 		try {
-		Client entity = repository.getOne(id);
-		entity.setName(dto.getName());
+		Client entity = repository.getById(id);
+		copyEntityDTO(entity, dto);
 		entity = repository.save(entity);
 		return new ClientDTO(entity);
 		}
@@ -69,4 +69,12 @@ public class ClientService {
 			throw new DatabaseException("Integrity violation");
 		}
 	}
+	
+	private void copyEntityDTO(Client entity, ClientDTO dto) {
+        entity.setName(dto.getName());
+        entity.setCpf(dto.getCpf());
+        entity.setIncome(dto.getIncome());
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setChildren(dto.getChildren());
+    }
 }
